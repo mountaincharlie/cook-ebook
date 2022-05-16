a0134946b2d19d5c03139ab171580bee70ff9d42
Charlie <ciharland@hotmail.co.uk>
2022-05-16 16:35:59 +0000

    test[test_models]: add test to check number_of_chefs_kisses method works correctly

--- cook_ebook/test_models.py
+++ cook_ebook/test_models.py
@@ -11,3 +11,14 @@ class TestModels(TestCase):
             chef = test_user,
         )
         self.assertEqual(recipe.public_status, 0)
+    
+
+    def test_number_of_chefs_kisses_method(self):
+        test_user = User.objects.create_user(username='user1', password='123')
+        test_user2 = User.objects.create_user(username='user2', password='123')
+        recipe = Recipe.objects.create(
+            title = 'test_recipe',
+            chef = test_user,
+        )
+        recipe.chefs_kisses.set([test_user, test_user2])
+        self.assertEqual(recipe.number_of_chefs_kisses(), 2)
