3eeb21feebfb4600f1a191ded604e20d43dfbd1a
Charlie <ciharland@hotmail.co.uk>
2022-05-16 16:28:02 +0000

    test[test_models]: create test for if recipe public_status is private by default

--- cook_ebook/test_models.py
+++ cook_ebook/test_models.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Recipe
 
-# Create your tests here.
+class TestModels(TestCase):
+
+    def test_recipes_private_by_default(self):
+        test_user = User.objects.create_user(username='user1', password='123')
+        recipe = Recipe.objects.create(
+            title = 'test_recipe',
+            chef = test_user,
+        )
+        self.assertEqual(recipe.public_status, 0)
