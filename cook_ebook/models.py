bd5cf1f6edbc7027be93d378313ae5fca55d9c8d
Charlie <ciharland@hotmail.co.uk>
2022-05-16 11:51:33 +0000

    feat[models.py] create Recipe class

--- cook_ebook/models.py
+++ cook_ebook/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+PUBLIC_STATUS = ((0, 'Private'), (1, 'Public'), (2, 'Awaits'))
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=150)
+    summary = models.TextField(blank=True)
+    cover_image = CloudinaryField('image', default='placecholder')
+    ingredients = models.ManyToManyField(Ingredient, related_name'recipe_item')
+    method = models.ManyToManyField(Method, related_name'recipe_step')
+    tags = models.ManyToManyField(Tag, blank=True, related_name'recipe_tag')
+    public_status = models.IntegerField(choices=PUBLIC_STATUS, default=0)
+    created_date = models.DateTimeField(auto_now_add=True)
+    chef = models.ForeignKey(User, on_delete=models.CASCADE, related_name'recipe_creator')
+    slug = models.SlugField(max_length=150, unique=True)
+    chefs_kisses = models.ManyToManyField(User, blank=True, related_name'recipe_chefs_kiss')
+
+    class Meta:
+        ordering = ['-created_date']
+    
+    def __str__(self):
+        return self.title
+    
+    def number_of_chefs_kisses(self):
+        return self.chefs_kisses.count()
+
+
+# class Ingredient
+
+# class Method
+
+# class Tags
