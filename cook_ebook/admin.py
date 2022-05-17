611c6e6d9890b14864a87c2449504e3fe557414a
Charlie <ciharland@hotmail.co.uk>
2022-05-17 10:08:33 +0000

    feat[admin]: customise Recipe display, search and actions in admin pannel

--- cook_ebook/admin.py
+++ cook_ebook/admin.py
@@ -1,7 +1,27 @@
 from django.contrib import admin
 from .models import Recipe, Ingredient, Method, Tag
 
-admin.site.register(Recipe)
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'public_status', 'chef', 'created_date')
+    search_fields = ('title', 'public_status', 'created_date', 'chef__username')
+    list_filter = ('public_status', 'chef__username', 'created_date')
+    actions = ['make_private', 'make_public', 'make_awaits']
+
+    # changes status to private
+    def make_private(self, request, queryset):
+        queryset.update(public_status=0)
+
+    # changes status to public
+    def make_public(self, request, queryset):
+        queryset.update(public_status=1)
+
+    # changes status to awaits
+    def make_awaits(self, request, queryset):
+        queryset.update(public_status=2)
+
+
 admin.site.register(Ingredient)
 admin.site.register(Method)
 admin.site.register(Tag)
