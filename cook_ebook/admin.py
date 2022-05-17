5d4e5be9f84a8c2dd222ec6afb1434e43f69198d
Charlie <ciharland@hotmail.co.uk>
2022-05-17 08:59:46 +0000

    feat[admin]: register the tables to the admin site

--- cook_ebook/admin.py
+++ cook_ebook/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Recipe, Ingredient, Method, Tag
 
-# Register your models here.
+admin.site.register(Recipe)
+admin.site.register(Ingredient)
+admin.site.register(Method)
+admin.site.register(Tag)
