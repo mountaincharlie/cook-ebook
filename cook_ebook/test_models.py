f8ee70c2cb4a5fd69df8bea5d85815a58b7dd32c
Charlie <ciharland@hotmail.co.uk>
2022-05-20 12:29:02 +0000

    test[models]: tag color field must be unique

--- cook_ebook/test_models.py
+++ cook_ebook/test_models.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth.models import User
-from .models import Recipe
+from .models import Recipe, Tag
+from django.db import IntegrityError
 
 class TestModels(TestCase):
 
@@ -22,3 +23,18 @@ class TestModels(TestCase):
         )
         recipe.chefs_kisses.set([test_user, test_user2])
         self.assertEqual(recipe.number_of_chefs_kisses(), 2)
+
+
+    def test_tag_color_must_be_unique(self):
+
+        with self.assertRaises(IntegrityError) as context:
+            tag1 = Tag.objects.create(
+                tag = 'tag1',
+                color = '#ffffff',
+            )
+            tag2 = Tag.objects.create(
+                tag = 'tag2',
+                color = '#ffffff',
+            )
+
+        self.assertTrue('UNIQUE constraint failed' in str(context.exception))
