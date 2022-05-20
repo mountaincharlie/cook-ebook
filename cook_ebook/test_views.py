a16c4c7097b1204e0ce429eb8c0e1c4c0cf3c7c2
Charlie <ciharland@hotmail.co.uk>
2022-05-20 12:30:42 +0000

    test[views]: index.html used correctly

--- cook_ebook/test_views.py
+++ cook_ebook/test_views.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import Recipe
 
-# Create your tests here.
+
+class TestViews(TestCase):
+
+    def test_get_tags_list(self):
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
