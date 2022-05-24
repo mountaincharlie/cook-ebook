1875c73add3496ef18f6d6a6903757be66e7bfe2
Charlie <ciharland@hotmail.co.uk>
2022-05-24 08:45:21 +0000

    feat: add classes and inline formsets for ingredients and method

--- cook_ebook/forms.py
+++ cook_ebook/forms.py
@@ -1,13 +1,30 @@
-from .models import Recipe
+from .models import Recipe, Ingredient, Method
 from django import forms
+
 
 class CreateRecipeForm(forms.ModelForm):
     class Meta:
         model = Recipe
-        fields = (
-            'title',
-            'summary',
-            'cover_image',
-            'tags',
-            'chef',
+        exclude = (
+            'public_status',
+            'created_date',
+            'chefs_kisses',
         )
+
+
+# credit: https://engineertodeveloper.com/getting-started-with-formsets-create-a-recipe-app/
+class AddIngredientForm(forms.ModelForm):
+    class Meta:
+        model = Ingredient
+        exclude = ('recipe',)
+
+
+class AddMethodForm(forms.ModelForm):
+    class Meta:
+        model = Method
+        exclude = ('recipe',)
+
+
+AddIngredientFormSet = forms.inlineformset_factory(Recipe, Ingredient, form=AddIngredientForm)
+
+AddMethodFormSet = forms.inlineformset_factory(Recipe, Method, form=AddMethodForm)
