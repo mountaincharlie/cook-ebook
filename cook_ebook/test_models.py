25336d0eb5e1893e4a32869950a31e975ee4b9b0
Charlie <ciharland@hotmail.co.uk>
2022-05-28 16:43:26 +0000

    fix: remove spaces around = operator

--- cook_ebook/test_models.py
+++ cook_ebook/test_models.py
@@ -8,8 +8,8 @@ class TestModels(TestCase):
     def test_recipes_private_by_default(self):
         test_user = User.objects.create_user(username='user1', password='123')
         recipe = Recipe.objects.create(
-            title = 'test_recipe',
-            chef = test_user,
+            title='test_recipe',
+            chef=test_user,
         )
         self.assertEqual(recipe.public_status, 0)
     
@@ -18,8 +18,8 @@ class TestModels(TestCase):
         test_user = User.objects.create_user(username='user1', password='123')
         test_user2 = User.objects.create_user(username='user2', password='123')
         recipe = Recipe.objects.create(
-            title = 'test_recipe',
-            chef = test_user,
+            title='test_recipe',
+            chef=test_user,
         )
         recipe.chefs_kisses.set([test_user, test_user2])
         self.assertEqual(recipe.number_of_chefs_kisses(), 2)
@@ -28,13 +28,13 @@ class TestModels(TestCase):
     def test_tag_color_must_be_unique(self):
 
         with self.assertRaises(IntegrityError) as context:
-            tag1 = Tag.objects.create(
-                tag = 'tag1',
-                color = '#ffffff',
+            tag1=Tag.objects.create(
+                tag='tag1',
+                color='#ffffff',
             )
-            tag2 = Tag.objects.create(
-                tag = 'tag2',
-                color = '#ffffff',
+            tag2=Tag.objects.create(
+                tag='tag2',
+                color='#ffffff',
             )
 
         self.assertTrue('UNIQUE constraint failed' in str(context.exception))
