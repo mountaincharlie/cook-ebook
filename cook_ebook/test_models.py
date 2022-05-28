2bd0f2ace0dd78ee4dd482f0d4ab695106d7acb6
Charlie <ciharland@hotmail.co.uk>
2022-05-28 20:14:43 +0000

    test[models]: add tests for slug must be unique and for the random_slug function

--- cook_ebook/test_models.py
+++ cook_ebook/test_models.py
@@ -38,3 +38,33 @@ class TestModels(TestCase):
             )
 
         self.assertTrue('UNIQUE constraint failed' in str(context.exception))
+
+    def test_slugs_must_be_unique(self):
+
+        with self.assertRaises(IntegrityError) as context:
+            test_user = User.objects.create_user(username='user1', password='123')
+            test_user2 = User.objects.create_user(username='user2', password='123')
+            recipe1=Recipe.objects.create(
+                title='title1',
+                chef=test_user,
+                slug='a-slug',
+            )
+            recipe2=Recipe.objects.create(
+                title='title2',
+                chef=test_user2,
+                slug='a-slug',
+            )
+        self.assertTrue('UNIQUE constraint failed' in str(context.exception))
+
+    def test_random_slug_function_generates_random_str(self):
+        test_user = User.objects.create_user(username='user1', password='123')
+        test_user2 = User.objects.create_user(username='user2', password='123')
+        recipe1=Recipe.objects.create(
+            title='title1',
+            chef=test_user,
+        )
+        recipe2=Recipe.objects.create(
+            title='title2',
+            chef=test_user2,
+        )
+        self.assertFalse(recipe1.slug == recipe2.slug)
