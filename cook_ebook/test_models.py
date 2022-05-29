87d75113146ddb0a7e2f9f5071b37dd59dedeae0
Charlie <ciharland@hotmail.co.uk>
2022-05-29 08:07:25 +0000

    fix: remove tag1= and tag2= since the vars were never used

--- cook_ebook/test_models.py
+++ cook_ebook/test_models.py
@@ -12,7 +12,6 @@ class TestModels(TestCase):
             chef=test_user,
         )
         self.assertEqual(recipe.public_status, 0)
-    
 
     def test_number_of_chefs_kisses_method(self):
         test_user = User.objects.create_user(username='user1', password='123')
@@ -24,23 +23,19 @@ class TestModels(TestCase):
         recipe.chefs_kisses.set([test_user, test_user2])
         self.assertEqual(recipe.number_of_chefs_kisses(), 2)
 
-
     def test_tag_color_must_be_unique(self):
-
         with self.assertRaises(IntegrityError) as context:
-            tag1=Tag.objects.create(
+            Tag.objects.create(
                 tag='tag1',
                 color='#ffffff',
             )
-            tag2=Tag.objects.create(
+            Tag.objects.create(
                 tag='tag2',
                 color='#ffffff',
             )
-
         self.assertTrue('UNIQUE constraint failed' in str(context.exception))
 
     def test_slugs_must_be_unique(self):
-
         with self.assertRaises(IntegrityError) as context:
             test_user = User.objects.create_user(username='user1', password='123')
             test_user2 = User.objects.create_user(username='user2', password='123')
