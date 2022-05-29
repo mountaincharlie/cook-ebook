d1b032a8f201d814f9242df374daf34e5a0bd394
Charlie <ciharland@hotmail.co.uk>
2022-05-29 12:12:10 +0000

    refactor: explicitly setting DEBUG to False

--- project_four/settings.py
+++ project_four/settings.py
@@ -29,8 +29,8 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+# For deployed project this variable in env.py is set to False
+DEBUG = False # 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['cook-ebook.herokuapp.com', 'localhost']
 
