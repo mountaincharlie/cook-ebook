95de8d9bbdd6a754ee1586e9f271a46bcd228513
Charlie <ciharland@hotmail.co.uk>
2022-05-29 08:05:48 +0000

    style: remove commented out code

--- cook_ebook/test_forms.py
+++ cook_ebook/test_forms.py
@@ -1,3 +1,62 @@
 from django.test import TestCase
+from .forms import CreateRecipeForm, AddIngredientFormSet, AddMethodFormSet
 
-# Create your tests here.
+
+class TestForms(TestCase):
+
+    def test_title_field_is_required(self):
+        form = CreateRecipeForm(
+            {
+                'title': ''
+            }
+        )
+        self.assertFalse(form.is_valid())
+
+    def test_chefs_kisses_is_not_required(self):
+        form = CreateRecipeForm(
+            {
+                'title': 'recipe1',
+                'chefs_kisses': None,
+            }
+        )
+        self.assertTrue(form.is_valid())
+
+    def test_tags_are_not_required(self):
+        form = CreateRecipeForm(
+            {
+                'title': 'recipe1',
+                'tags': None,
+            }
+        )
+        self.assertTrue(form.is_valid())
+
+    def test_cover_image_is_not_required(self):
+        form = CreateRecipeForm(
+            {
+                'title': 'recipe1',
+                'cover_image': None,
+            }
+        )
+        self.assertTrue(form.is_valid())
+
+    def test_ingredients_are_not_required(self):
+        form = AddIngredientFormSet(
+            {
+                'items-TOTAL_FORMS': '0',
+                'items-INITIAL_FORMS': '0',
+                'items-MIN_NUM_FORMS': '0',
+                'items-MAX_NUM__FORMS': '1000',
+            }
+        )
+        self.assertTrue(form.is_valid())
+
+    def test_method_setps_are_not_required(self):
+        form = AddMethodFormSet(
+            {
+                'steps-TOTAL_FORMS': '0',
+                'steps-INITIAL_FORMS': '0',
+                'steps-MIN_NUM_FORMS': '0',
+                'steps-MAX_NUM__FORMS': '1000',
+            }
+        )
+        self.assertTrue(form.is_valid())
