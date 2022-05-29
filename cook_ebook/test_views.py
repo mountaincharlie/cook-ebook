ac17e7b26e550856c3d6b5377461f5855b2f173f
Charlie <ciharland@hotmail.co.uk>
2022-05-29 08:10:09 +0000

    test[test_views.py]: add 5 new tests

--- cook_ebook/test_views.py
+++ cook_ebook/test_views.py
@@ -1,10 +1,59 @@
 from django.test import TestCase, Client
-from .models import Recipe
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import Tag, Recipe
 
 
 class TestViews(TestCase):
 
-    def test_get_tags_list(self):
-        response = self.client.get('')
+    def test_tags_list_GET(self):
+        client = Client()
+        response = client.get(reverse('home'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'index.html')
+
+    def test_recipe_search_GET(self):
+        client = Client()
+        data = {'search': 'chicken'}
+        response = client.get(reverse('search_recipes'), data)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+
+    def test_recipe_by_tag_GET(self):
+        client = Client()
+        Tag.objects.create(
+            tag='Chicken',
+            color='#ffffff',
+        )
+        response = client.get(reverse('search_tags', args=['Chicken']))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'index.html')
+
+    def test_recipe_details_GET(self):
+        client = Client()
+        test_user = User.objects.create_user(username='user1', password='123')
+        recipe = Recipe.objects.create(
+            title='test_recipe',
+            chef=test_user,
+            slug=12345678901234567890,
+        )
+        response = client.get(reverse('recipe_details', args=[recipe.slug]))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'recipe_details.html')
+
+    # tests requiring request.user.id
+    # test_my_ebook_tag_search_GET 
+    # test_edit_recipe_GET
+    # test_delete_recipe_POST ...
+
+    def test_create_recipe_GET(self):
+        client = Client()
+        response = client.get(reverse('create_recipe'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'create_recipe.html')
+
+    def test_about_page(self):
+        client = Client()
+        response = client.get(reverse('about'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'about.html')
