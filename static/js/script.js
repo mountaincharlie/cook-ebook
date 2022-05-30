eaee10902ea808a0f369a7d7c1619dd5e1808548
Charlie <ciharland@hotmail.co.uk>
2022-05-30 19:16:04 +0000

    style: replace inline comments with function docstrings

--- static/js/script.js
+++ static/js/script.js
@@ -2,16 +2,12 @@
 
 // ---- GLOBAL VARS
 
-// getting the 'id_items-TOTAL_FORMS' div so that I can update this number everytime a new ingredient is added
+// getting the 'id_items-TOTAL_FORMS' and 'id_steps-TOTAL_FORMS' divs so that I can update these numbers everytime a new ingredient item/method step is added
 let totalIngredients = document.getElementById('id_items-TOTAL_FORMS');
-
-// setting empty list to add used id's too to avoid duplication after items deleted
-let usedIngredientIds = []; 
-
-// getting the 'id_steps-TOTAL_FORMS' div so that I can update this number everytime a new method is added
 let totalMethods = document.getElementById('id_steps-TOTAL_FORMS');
 
-// setting empty list to add used id's too to avoid duplication after steps are deleted
+// setting empty list to add used id's too to avoid duplication after ingredient items/method steps are deleted
+let usedIngredientIds = []; 
 let usedMethodIds = []; 
 
 
@@ -47,7 +43,14 @@ document.addEventListener('DOMContentLoaded', function(){
 });
 
 
-// functino to add d-none class to all formset delete checkboxes so they're not visible on the page but can be accessed by JavaScript
+/**
+ * @name hideFormsetDeleteCheckboxes
+ * @description gets all of the elements with the class name 'checkboxinput'
+ * then uses a for loop to add the bootstrap 'd-none' class to hide the checkboxes.
+ * This hides all of the inline formsets' delete checkboxes without removing
+ * the ability to delete formset items and the ability to access them with
+ * javascript
+ */
 function hideFormsetDeleteCheckboxes(){
     const checkboxesToHide = document.getElementsByClassName('checkboxinput');
     for (const checkbox of checkboxesToHide){
@@ -58,223 +61,292 @@ function hideFormsetDeleteCheckboxes(){
 
 // ---- FUNCTIONS FOR RECIPE INGREDIENT ITEMS
 
-
-// function to get all existing ingredient forms and add custom delete button with related id attribute
+/**
+ * @name addDeleteBtnToExistingIngredients
+ * @description this is used when editing recipes.
+ * Gets all existing ingredient forms.
+ * Uses a for loop to add a unique id to each of the ingredient forms as
+ * well as the custom delete button which has an id related by an integer
+ * 'n' to its ingredient form.
+ * This used id is then added to the usedIngredientIds list.
+ */
 function addDeleteBtnToExistingIngredients(){
-    // getting any existing ingredient forms (relevant for when recipes are being edited)
+    
     const existingIngredientForms = document.getElementsByClassName('ingredient-form');
 
     // counter for the loop
     var n = 0;
-    // giving an id to all of the existing ingredient forms and adding a custom delete button with and id thats related to the form
     for (const div of existingIngredientForms) {
+
         div.setAttribute('id', `ingredient-form-${n}`);
-        // getting a copy of the delete ingredient button
+        
         const newDeleteIngredientBtn = document.getElementById('new-delete-ingredient-button').cloneNode(true);
-        // give delete button a matching id
+        
         newDeleteIngredientBtn.setAttribute('id', `delete-ingredient-button-${n}`);
-        // add the button to the div
+        
         div.appendChild(newDeleteIngredientBtn);
-        // add the used id to usedIngredientIds
+        
         const usedId = div.children[0].getAttribute('id');
         usedIngredientIds.push(usedId);
+
         n++;
     }
 }
 
 
-// function to apply delete button event listener to all current delete ingredient buttons
+/**
+ * @name addEvListenerToDeleteIngredientBtns
+ * @description gets all of the buttons with class name 'delete-ingredient'
+ * and uses a for loop to add the deleteIngredient() click Event Listener
+ */
 function addEvListenerToDeleteIngredientBtns(){
-    // applying delete button event listener to all delete ingredient buttons
     let deleteIngredientBtn = document.getElementsByClassName('delete-ingredient');
-    // looping through the buttons to assign EventListener
     for (const btn of deleteIngredientBtn){
         btn.addEventListener("click", deleteIngredient);
     }
 }
 
 
-// add ingredient function [called by addIngredient]
+/**
+ * @name addIngredient
+ * @description called by addIngredientBtn() click Event Listener 
+ * (which was assigned in the DOM load Event Listner)
+ * If statement to prevent any default behaviour.
+ * Gets the length of usedIngredientIds which is the number which
+ * should be used to create the next unique ingredient form id.
+ * Gets the ingredient form list, to append to, by its id of 'ingredient-list'.
+ * Gtes a copy of the hidden ingredient form which has an id of
+ * 'new-ingredient-form'.
+ * Gives the new ingredient form the 'ingredient-form' class to match the
+ * others.
+ * Gives the new ingredient form its unique id using ingredientFormId.
+ * Gets the new ingredient form's custom delete button and gives it its
+ * unique id using ingredientFormId.
+ * Pushes the used id into usedIngredientIds array.
+ * Uses a regular expression to change __prefix__ to the forms number so that
+ * the name and id for each form input will be related and unique as more are
+ * added.
+ * Updates the value for totalIngredients by 1 and adds the new form to
+ * ingredientFormList.
+ * Calls the addEvListenerToDeleteIngredientBtns function to add an event
+ * listener to the new delete ingredient button
+ */
 function addIngredient(ev){
-    // preventing any potential default action
     if (ev){
         ev.preventDefault();
     }
-    // length of usedIngredientIds = next unused id which will definetly be unique
+    
     const ingredientFormId = usedIngredientIds.length;
 
-    // getting the ingredient form list to append to
     const ingredientFormList = document.getElementById('ingredient-list');
 
-    // getting the empty form and making a copy
     const newEmptyIngredientForm = document.getElementById('new-ingredient-form').cloneNode(true);
 
-    // setting class for the new ingredient form to match the others
     newEmptyIngredientForm.setAttribute('class', 'ingredient-form');
-    // setting unique id for new ingredient forms
     newEmptyIngredientForm.setAttribute('id', `ingredient-form-${ingredientFormId}`);
 
-    // getting delete button and setting id for the delete button using the numberOfIngredientForms
     let newDeleteIngredientBtn = newEmptyIngredientForm.lastElementChild;
-    // newDeleteIngredientBtn.classList.add('delete-ingredient'); 
     newDeleteIngredientBtn.setAttribute('id', `delete-ingredient-button-${ingredientFormId}`);
 
-    // pushing the used id into ingredientFormId array
     usedIngredientIds.push(ingredientFormId);
     
-    // using regular expression to change __prefix__ to the forms number so that the name and id for each form input will be unique as more are added
     const regexp = new RegExp('__prefix__', 'g');
     newEmptyIngredientForm.innerHTML = newEmptyIngredientForm.innerHTML.replace(regexp, ingredientFormId);
-    // updating value for totalIngredients by 1
     totalIngredients.setAttribute('value', ingredientFormId + 1);
-    // adding the new form to ingredientFormList
     ingredientFormList.append(newEmptyIngredientForm);
 
-    // calling function to add event listeners to all delete ingredient buttons
     addEvListenerToDeleteIngredientBtns();
-    
 }
 
 
-// delete ingredient function [called by deleteIngredientBtn]
+/**
+ * @name deleteIngredient
+ * @description called by addEvListenerToDeleteIngredientBtns() click Event
+ * Listener
+ * If statement to prevent any default behaviour.
+ * Gets the hidden inline formset's default delete checkbox by using a query
+ * selector on the button's parent element to find all of its children with the
+ * 'form-check' class name and then finding which has the 'form-check-input'
+ * class.
+ * Sets the checkbox as checked so that on submission the ingredient will
+ * be removed from the recipe.
+ * Applys the bootstrap 'd-none' class to the deleted ingredient form so that
+ * its no longer visible on the page.
+ */
 function deleteIngredient(ev){
-    // preventing any potential default action
     if (ev){
         ev.preventDefault();
     }
-    // getting the inline formset delete checkbox
+
     const checkbox = this.parentElement.querySelector('.form-check').querySelector('.form-check-input');
-    // setting it as checked so that on submission the ingredient will be removed from the recipe
     checkbox.checked = true;
 
-    // hiding the ingredient and its delete button
     this.parentElement.classList.add('d-none');
 }
 
 
 // ---- FUNCTIONS FOR RECIPE METHOD STEPS
 
-// function to get existing method forms and add custom delete button with related id attribute
+/**
+ * @name addDeleteBtnToExistingMethods
+ * @description this is used when editing recipes.
+ * Gets all existing method forms.
+ * Uses a for loop to add a unique id to each of the method forms as
+ * well as the custom method delete button which has an id related by an integer
+ * 'n' to its method form.
+ * This used id is then added to the usedMethodIds list.
+ */
 function addDeleteBtnToExistingMethods(){
 
-    // getting any existing method forms (relevant for when recipes are being edited)
     const existingMethodForms = document.getElementsByClassName('method-form');
-    // counter variable for the loop
+
+    // counter for the loop
     var n = 0;
     for (const div of existingMethodForms) {
-        div.setAttribute('id', `method-form-${n}`);
-        // getting a copy of the delete method button
-        const newDeleteMethodBtn = document.getElementById('new-delete-method-button').cloneNode(true);
-        // give delete button a matching id
-        newDeleteMethodBtn.setAttribute('id', `delete-method-button-${n}`);
-        // add the button to the div
-        div.appendChild(newDeleteMethodBtn);
-        n++;
-    }
 
-    // console.log('existing forms',existingMethodForms);
-    for (const div of existingMethodForms) {
+        div.setAttribute('id', `method-form-${n}`);
+
+        const newDeleteMethodBtn = document.getElementById('new-delete-method-button').cloneNode(true);
+        
+        newDeleteMethodBtn.setAttribute('id', `delete-method-button-${n}`);
+
+        div.appendChild(newDeleteMethodBtn);
+
         const usedId = div.children[0].getAttribute('id');
         usedMethodIds.push(usedId);
-    }
 
+        n++;
+    }
 }
 
 
-// function to apply delete button event listener to all delete method buttons
+/**
+ * @name addEvListenerToDeleteMethodBtns
+ * @description gets all of the buttons with class name 'delete-method'
+ * and uses a for loop to add the deleteMethod() click Event Listener
+ */
 function addEvListenerToDeleteMethodBtns(){
-    // adding event listener to all delete buttons
     let deleteMethodBtn = document.getElementsByClassName('delete-method');
-    // looping through the buttons to assign EventListener
     for (const btn of deleteMethodBtn){
         btn.addEventListener("click", deleteMethod);
     }
 }
 
 
-// add method function [called by addMethod]
+/**
+ * @name addMethod
+ * @description called by addMethodBtn() click Event Listener 
+ * (which was assigned in the DOM load Event Listner)
+ * If statement to prevent any default behaviour.
+ * Gets the length of methodFormId which is the number which
+ * should be used to create the next unique method form id.
+ * Gets the method form list, to append to, by its id of 'method-list'.
+ * Gtes a copy of the hidden method form which has an id of
+ * 'new-method-form'.
+ * Gives the new method form the 'method-form' class to match the
+ * others.
+ * Gives the new method form its unique id using methodFormId.
+ * Gets the new method form's custom delete button and gives it its
+ * unique id using methodFormId.
+ * Pushes the used id into usedMethodIds array.
+ * Uses a regular expression to change 'items-__prefix__-item' to
+ * 'steps-${methodFormId}-step' so that the name and id for each method form
+ * input will be unique as more are added and never the same as the ingredients
+ * forms. In this case the id on the hidden formset delete checkboxes also
+ * needs to be updated.
+ * The new form's label also has to be changed from Item* to Step*.
+ * Updates the value for totalMethods by 1 and adds the new form to
+ * methodFormList.
+ * Calls the addEvListenerToDeleteMethodBtns function to add an event
+ * listener to the new delete method button.
+ */
 function addMethod(ev){
-    // preventing any potential default action
     if (ev){
         ev.preventDefault();
     }
-    // length of usedMethodIds = next unused id which will definetly be unique
+
     const methodFormId = usedMethodIds.length;
 
-    // getting the method form list to append to
     const methodFormList = document.getElementById('method-list');
 
-    // getting the empty form and making a copy
     const newEmptyMethodForm = document.getElementById('new-method-form').cloneNode(true);
 
-    // setting class for the new method form to match the others
     newEmptyMethodForm.setAttribute('class', 'method-form');
-    // setting unique id for new method forms
     newEmptyMethodForm.setAttribute('id', `method-form-${methodFormId}`);
 
-    // getting delete button and setting id for the delete button using the methodFormId
     let newDeleteMethodBtn = newEmptyMethodForm.lastElementChild;
     newDeleteMethodBtn.setAttribute('id', `delete-method-button-${methodFormId}`);
 
-    // pushing the used id into methodFormId array
     usedMethodIds.push(methodFormId);
 
-    // using regular expression to change 'items-__prefix__-item' to 'steps-${methodFormId}-step' so that the name and id for each method form input will be unique as more are added and never the same as the ingredients forms. In this case the id on the hidden formset delete checkboxes also needs to be updated 
     const regexp = new RegExp('items-__prefix__-item', 'g');
     newEmptyMethodForm.innerHTML = newEmptyMethodForm.innerHTML.replace(regexp, `steps-${methodFormId}-step`);
-    // get child form-check
+
     const newFormsetCheckbox = newEmptyMethodForm.querySelector('.form-check');
     const regexpOnCheckbox = new RegExp('items-__prefix__-DELETE', 'g');
     newFormsetCheckbox.innerHTML = newFormsetCheckbox.innerHTML.replace(regexpOnCheckbox, `steps-${methodFormId}-DELETE`);
     
-
-    // changing label value from Item* to Step*
     newEmptyMethodForm.children[0].children[0].innerText = 'Step*';
-    // updating value for totalMethods by 1
+    
     totalMethods.setAttribute('value', methodFormId + 1);
-    // adding the new form to methodFormList
     methodFormList.append(newEmptyMethodForm);
 
-    // calling function to add event listeners to all delete method buttons
     addEvListenerToDeleteMethodBtns();
-    
 }
 
 
-// delete method function [called by deleteMethodBtn]
+/**
+ * @name deleteMethod
+ * @description called by addEvListenerToDeleteMethodBtns() click Event
+ * Listener.
+ * If statement to prevent any default behaviour.
+ * Gets the hidden inline formset's default delete checkbox by using a query
+ * selector on the button's parent element to find all of its children with the
+ * 'form-check' class name and then finding which has the 'form-check-input'
+ * class.
+ * Sets the checkbox as checked so that on submission the method will
+ * be removed from the recipe.
+ * Applys the bootstrap 'd-none' class to the deleted method form so that
+ * its no longer visible on the page.
+ */
 function deleteMethod(ev){
-    // preventing any potential default action
     if (ev){
         ev.preventDefault();
     }
-    // getting the inline formset delete checkbox
+    
     const checkbox = this.parentElement.querySelector('.form-check').querySelector('.form-check-input');
-    // setting it as checked
+   
     checkbox.checked = true;
 
-    // hiding the method and its delete button
     this.parentElement.classList.add('d-none');
 }
 
 
 // ---- FUNCTION FOR CHECKING EXISTING TAG CHECKBOXES
 
-// function for ticking the check boxes of existing tags function [called in DOM load]
+/**
+ * @name existingTagsCheck
+ * @description this is used when editing recipes.
+ * Gets the hidden element containing all the existing tags for the recipe by
+ * class name 'existing-tag'.
+ * Creates an empty list to put the existing tag ids into.
+ * Uses a for loop to add the existing tag ids into the listOfExistingTagValues
+ * as integers.
+ * Gets all of the elements with class name 'form-check-input'.
+ * Uses a for loop to check if any of these check inputs have a value which is
+ * in listOfExistingTagValues and if so, their checkbox is checked.
+ */
 function existingTagsCheck(){
 
-    // getting all the hidden elements containing all the existing tags for the recipe
     const existingTags = document.getElementsByClassName('existing-tag');
-    // creating an empty list to put the existing tag ids into
     const listOfExistingTagValues = [];
 
-    // adding the existing tag ids into the list as integers
     for(const tag of existingTags){
         listOfExistingTagValues.push(parseInt(tag.innerHTML));
     }
 
-    // getting all the elements with class 'form-check-input'
     const allCheckInputs = document.getElementsByClassName('form-check-input');
-    // checking if any of the check inputs have a value which is in listOfExistingTagValues and if so, their checkbox is checked
+
     for(const checkBox of allCheckInputs){
         if (listOfExistingTagValues.includes(parseInt(checkBox.getAttribute('value')))){
             checkBox.checked = true;
